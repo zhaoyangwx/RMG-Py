522de0efd75faaca692278f1041ecb2dcd34dd2b
Yen-Ting Wang <yentingw@mit.edu>
2021-10-22 14:26:38 -0400

    allow user to specify cutting threshold when new species are generated

--- rmgpy/rmg/input.py
+++ rmgpy/rmg/input.py
@@ -1278,6 +1278,7 @@ def generated_species_constraints(**kwargs):
         'maximumSingletCarbenes',
         'maximumCarbeneRadicals',
         'allowSingletO2',
+        'speciesCuttingThreshold',
     ]
 
     for key, value in kwargs.items():
